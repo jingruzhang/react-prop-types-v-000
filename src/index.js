b7bab646940f8a450b754eeff502817e7df40bbb
JingruZhang <zhang.jingru.93@gmail.com>
2018-03-05 17:18:03 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 // Code Goes Here
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Order from './components/order';
+
+ReactDOM.render(<Order />, document.querySelector('#root'))
